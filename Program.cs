61e50caec24f346f795195303df5283d235d3218
“rajeshjoshph” <rajeshkannan1999.rk@gmail.com>
2023-02-23 20:44:08 -0800

    ADD: Compare Two Lines

--- Program.cs
+++ Program.cs
@@ -1,37 +1,43 @@
-﻿namespace UC2_EqualityOfTwoLines
+﻿namespace UC3_CompareTwoLine
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            int point_x1;
-            int point_x2, point_y1, point_y2, line1, line2;
+            int point_x1 = 0;
+            int point_x2 = 0, point_y1 = 0, point_y2 = 0;
+            int line1, line2;
             double length;
-            //bool value1, value2;
 
+            Console.WriteLine("Enter 1st Value of Point: ");
+            point_x1 = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Enter the First Value of Point: ");
-            point_x1=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter 2nd Value of Point: ");
+            point_x2 = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Enter the Secount value of Point: ");
-            point_x2= Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter 3rd Value of Point: ");
+            point_y1 = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Enter the thires Value of Point: ");
-            point_y1= Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine("Enter the Forth Value of Point: ");
-            point_y2= Convert.ToInt32(Console.ReadLine());
-
-            
+            Console.WriteLine("Enter 4th Value of Point: ");
+            point_y2 = Convert.ToInt32(Console.ReadLine());
 
             line1 = point_x2 - point_x1;
-            line2=point_y2- point_y1;
+            line2 = point_y2 - point_y1;
 
-            length = Math.Sqrt(Math.Pow(line1,2)+Math.Pow(line2,2));
-            Console.WriteLine("Length of the line:"+ length);
-                                   
-            Console.WriteLine(length.Equals(line1));
-            Console.WriteLine(length.Equals(line2));
+            length = Math.Sqrt(Math.Pow(line1, 2) + Math.Pow(line2, 2));
+
+            if (point_x1>point_x2 && point_y1>point_y2)
+            {
+                Console.WriteLine("one line Are greatest");
+            }
+            else if (point_x1<point_x2 && point_y1<point_y2)
+            {
+                Console.WriteLine("One line are lessthen");
+            }
+            else
+            {
+                Console.WriteLine("Two line Are Equal");
+            }
 
         }
     }
